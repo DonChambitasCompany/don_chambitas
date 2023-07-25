25af5921b13ab4b8de50d2812bd3ab0336f37745
DonChambitasCompany <139256936+DonChambitasCompany@users.noreply.github.com>
2023-07-24 20:00:49 -0600

    Modal para no repetir en todos
    Únicamente con un include en los esos para que aparezca el modal y no repetir codigo

---
+++ categorias/modal.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    
+    <!--MODAL DE TRABAJADOR-->
+    <!-- Button trigger modal -->  
+  <!-- Modal -->
+  <div class="modal fade " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">TRABAJADOR</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+                <div class="container">
+                    <div class="image">
+                      <img src="https://st.depositphotos.com/1518767/3618/i/950/depositphotos_36183013-stock-photo-smiling-plumber-holding-wrench-sitting.jpg" alt="Imagen" width="100%" height="180px">
+                    </div>
+                    <div class="info">
+                      <div class="info-item">
+                        <span class="name"><strong> Gabriel Ramírez Arroyo</strong></span>
+                      </div>
+                      <div class="info-item">
+                        <span class="specialty"><em>Fontanero especializado</em></span>
+                      </div>
+                      <div class="info-item">
+                        <span class="price">$99.99</span>
+                      </div>
+                    </div>
+                  </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+          <button type="button" class="btn btn-primary">Pedir Servicio</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</html>
