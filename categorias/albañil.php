e9b9d77371b203cb925752721aa515a3bf74f614
DonChambitasCompany <139256936+DonChambitasCompany@users.noreply.github.com>
2023-07-24 18:34:40 -0600

    Add files via upload

---
+++ categorias/albañil.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../styles.css">
+</head>
+<header>
+    <?php include '../header.php'; ?>
+</header>
+<body>
+    <div class="container">
+        <h2 class="mt-lg-5 mb-4" id="categorias">SELECCIONA AL CHAMBEADOR QUE TE GUSTARÍA SOLICITAR</h2>
+        <div class="row">
+            <div class="col-md-3 mb-4">
+                <div class="card d-flex flex-column align-items-center pulso">
+                    <img src="../assets/albañilperfil.jpg" class="card-img-top small-image" alt="Imagen 1">
+                    <div class="card-body">
+                        <h5 class="card-title">Albañìl, Puebla, México 3.5 ★</h5>
+                        <p class="card-text">a 2.67km de distancia</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 mb-4">
+                <div class="card d-flex flex-column align-items-center pulso">
+                    <img src="../assets/albañilperfil.jpg" class="card-img-top small-image" alt="Imagen 1">
+                    <div class="card-body">
+                        <h5 class="card-title">Albañíl, Puebla, México 2.9 ★</h5>
+                        <p class="card-text">a 2.87km de distancia</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 mb-4">
+                <div class="card d-flex flex-column align-items-center pulso">
+                    <img src="../assets/albañilperfil.jpg" class="card-img-top small-image" alt="Imagen 1">
+                    <div class="card-body">
+                        <h5 class="card-title">Albañíl, Puebla, México 4.0 ★</h5>
+                        <p class="card-text">a 2.67km de distancia</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 mb-4">
+                <div class="card d-flex flex-column align-items-center pulso">
+                    <img src="../assets/albañilperfil.jpg" class="card-img-top small-image" alt="Imagen 1">
+                    <div class="card-body">
+                        <h5 class="card-title">Albañíl, Puebla, México 3.5 ★</h5>
+                        <p class="card-text">a 2.67km de distancia</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 mb-4">
+                <div class="card d-flex flex-column align-items-center pulso">
+                    <img src="../assets/albañilperfil.jpg" class="card-img-top small-image" alt="Imagen 1">
+                    <div class="card-body">
+                        <h5 class="card-title">Albañíl, Puebla, México 3.5 ★</h5>
+                        <p class="card-text">a 2.67km de distancia</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 mb-4">
+                <div class="card d-flex flex-column align-items-center pulso">
+                    <img src="../assets/albañilperfil.jpg" class="card-img-top small-image" alt="Imagen 1">
+                    <div class="card-body">
+                        <h5 class="card-title">Albañíl, Puebla, México 2.0 ★</h5>
+                        <p class="card-text">a 2.67km de distancia</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 mb-4">
+                <div class="card d-flex flex-column align-items-center pulso">
+                    <img src="../assets/albañilperfil.jpg" class="card-img-top small-image" alt="Imagen 1">
+                    <div class="card-body">
+                        <h5 class="card-title">Albañíl, Puebla, México 4.8 ★</h5>
+                        <p class="card-text">a 2.67km de distancia</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 mb-4">
+                <div class="card d-flex flex-column align-items-center pulso">
+                    <img src="../assets/albañilperfil.jpg" class="card-img-top small-image" alt="Imagen 1">
+                    <div class="card-body">
+                        <h5 class="card-title">Albañíl, Puebla, México 5.0 ★</h5>
+                        <p class="card-text">a 2.67km de distancia</p>
+                    </div>
+                </div>
+            </div>
+            
+    </div>
+</body>
+</html>
