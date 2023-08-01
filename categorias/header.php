a3ee72641fdbf9d3e09eeab96726082785344d45
DonChambitasCompany <139256936+DonChambitasCompany@users.noreply.github.com>
2023-08-01 13:37:03 -0600

    Add files via upload

---
+++ categorias/header.php
@@ -0,0 +1,22 @@
+<header id="header_prin">
+            <nav>
+                <div class="logo">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col">
+                                <a href="../index.php"><img src="../assets/logo_principal.png" alt="LogoPrincipal" id="logo-principal"></a>
+                            </div>
+                            <div class="col">
+                                <h2 style="color: black;">Don Chambitas</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <ul class="nav-links">
+                    <li><a href="#trabajo">Top trabajadores</a></li>
+                    <li><a href="#categorias">Categorías</a></li>
+                    <li><a href="#">¡Quiero trabajar!</a></li>
+                </ul>
+                <button class="btn" id="boton"><a href="../register/register.php">Solicitar Servicio</a></button>
+            </nav>
+</header>
