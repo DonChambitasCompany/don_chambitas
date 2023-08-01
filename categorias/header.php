264921525fea189876c4537264d473acb4f513c4
DonChambitasCompany <139256936+DonChambitasCompany@users.noreply.github.com>
2023-08-01 13:40:36 -0600

    Update header.php

--- categorias/header.php
+++ categorias/header.php
@@ -4,7 +4,7 @@
                     <div class="container">
                         <div class="row">
                             <div class="col">
-                                <a href="../index.php"><img src="../assets/logo_principal.png" alt="LogoPrincipal" id="logo-principal"></a>
+                                <a href="index.php"><img src="assets/logo_principal.png" alt="LogoPrincipal" id="logo-principal"></a>
                             </div>
                             <div class="col">
                                 <h2 style="color: black;">Don Chambitas</h2>
@@ -16,7 +16,20 @@
                     <li><a href="#trabajo">Top trabajadores</a></li>
                     <li><a href="#categorias">Categorías</a></li>
                     <li><a href="#">¡Quiero trabajar!</a></li>
+                
+                <?php
+                    if (isset($_SESSION['usuario'])){
+                        $perfil = '<a href="profiles/usuario.php">Mi perfil</a>';
+                        $boton = '<a href="cerrar.php">Cerrar Sesión</a>';
+                    }
+                    else{
+                        $perfil = '';
+                        $boton = '<a href="register/login.php">Iniciar Sesión</a>';
+                    }?>
+                <li><a href="profiles/usuario.php"><?php echo $perfil; ?> </li>
                 </ul>
-                <button class="btn" id="boton"><a href="../register/register.php">Solicitar Servicio</a></button>
+                <li>
+                <button class="btn" id="boton"><a href="register/login.php"><?php echo $boton; ?></button>
+                </li>
             </nav>
 </header>
