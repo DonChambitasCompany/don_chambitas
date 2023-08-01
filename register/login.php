e7546874a8ca96915c491f55fd1685b3566d7be0
DonChambitasCompany <139256936+DonChambitasCompany@users.noreply.github.com>
2023-08-01 12:33:28 -0600

    inicio de sesión
    Se crea inicio de sesión con php, se hace para que no pueda acceder al perfil si no ha iniciado sesión además de que hasta que de en cerrar sesión se cierra

--- register/login.php
+++ register/login.php
@@ -1,9 +1,43 @@
+<?php
+session_start();
+
+if (isset($_SESSION['usuario'])){//comprobar si ya está la sesión
+    header('Location: ../profiles/usuario.php');//si es así va pal index
+}
+$errores = '';
+
+// se crean variables
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $correo = filter_var(strtolower($_POST['correo']),FILTER_SANITIZE_STRING);
+    $password = $_POST['password'];
+    $password = hash('sha512', $password);
+
+    try{
+        $conexion = new PDO('mysql:host=localhost;dbname=don_chambitas','root','75628491d');
+    }  
+    catch (PDOException $e){
+        echo "Error:" .$e->getMessage();
+    }
+
+    $statement = $conexion->prepare('SELECT * FROM cuentas where correo_electronico = :correo AND my_password = :pass');
+    $statement ->execute(array(':correo' => $correo,':pass' => $password ));
+
+    $resultado = $statement->fetch();
+    if ($resultado != false){
+        $_SESSION['usuario'] = $correo;
+        header('Location: ../profiles/usuario.php');
+    }
+    else{
+        $errores .= '<li>Datos incorrectos</li>';
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
+    <title>Login</title>
     <link rel="stylesheet" href="register.css" type="text/css">
     <link rel="stylesheet" href="login.css" type="text/css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
@@ -20,24 +54,36 @@
                 <div class="col-md-6">
                     <div class="wrapper">
                             <h1>INICIAR SESIÓN</h1>             
-                        <form action="../profiles/usuario.html">
+                        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?> " method="POST">
                             <div class="input-row">
                                 <div class="input-box">
                                     <span class="icon"><ion-icon name="mail-outline"></ion-icon></span>
-                                    <input type="text" required >
+                                    <input type="text" required name="correo" >
                                     <label for=""> Correo</label>
                                 </div>
                                 <div class="input-box">
                                         <span class="icon"><ion-icon name="lock-closed-outline"></ion-icon></span>
-                                        <input type="password" required >
+                                        <input type="password" required name="password">
                                         <label for=""> Contraseña</label>
                                     </div>
                             </div>
-                            <button type="submit" class="buton" id="" onclick="validarFormularioInicioSesion()">Continuar</button>
+                            <button type="submit" class="buton" id="" onclick="login.submit()">Continuar</button>
                             <br>
                             <br>
                             
-                            <a href="register.php"><button type="button" class="buton buton-black">Volver</button></a>
+                            <a href="../index.html"><button type="button" class="buton buton-black">Volver</button></a>
+                            <br><br>
+                            <a href="register.html"><button type="button" class="buton buton-registro">¿Aún no tienes una cuenta?
+                                <br><strong>Registrate</strong></button></a>
+                            
+                            <?php if (!empty($errores)):?>
+                            <div class="error">
+                                <ul>
+                                    <?php echo $errores;?>
+                                </ul>
+                            </div>
+                        <?php endif;?>
+                                
                         </form>
                     </div>
                     </div>
