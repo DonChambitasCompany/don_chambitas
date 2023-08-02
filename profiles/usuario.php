6c9ef4cc8329470e271e59cfb106bb53d546ad13
DonChambitasCompany <139256936+DonChambitasCompany@users.noreply.github.com>
2023-08-01 20:25:44 -0600

    Update and rename usuario.html to usuario.php

--- profiles/usuario.php
+++ profiles/usuario.php
@@ -1,3 +1,10 @@
+<?php 
+session_start();
+if (!isset($_SESSION['usuario'])){//comprobar si ya está la sesión
+    header('Location: ../register/login.php');
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
