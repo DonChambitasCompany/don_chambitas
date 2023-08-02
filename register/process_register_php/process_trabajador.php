23ac56c97bcf83a0353683a74d193303098dc243
DonChambitasCompany <donchambitas2003@gmail.ccom>
2023-08-02 16:13:26 -0600

    changes: deleted:    register/inf_trabajador.css modified:   register/process_register_php/process_trabajador.php new file:   register/register_trabajador.css modified:   register/register_trabajador.php

--- register/process_register_php/process_trabajador.php
+++ register/process_register_php/process_trabajador.php
@@ -4,6 +4,7 @@ require_once "conexion.php";
 
 // Obtenemos los datos del formulario
 
+
 //tabla usuarios
 $nombre_usuario = $_POST["nombre_usuario"];
 $apellido_paterno = $_POST["apellido_paterno"];
@@ -31,6 +32,25 @@ $estado_o_provincia = $_POST["estado_o_provincia"];
 
 //tabla codigo postal
 $numero_codigo_postal = $_POST["numero_codigo_postal"];
+
+//tabla documentacion
+$numero_curp = $_POST["numero_curp"];
+$numero_cartilla_militar = $_POST["numero_cartilla_militar"];
+$numero_pasaporte = $_POST["numero_pasaporte"];
+$nombre_tipo_residencia = $_POST["nombre_tipo_residencia"];
+$doc_extranjero = $_POST["doc_extranjero"];
+$licencia_manejo = $_POST["licencia_manejo"];
+$nombre_tipo_licencia= $_POST["nombre_tipo_licencia"];
+$numero_licencia = $_POST["numero_licencia"];
+
+//tabla educacion
+$nombre_universidad = $_POST["nombre_universidad"];
+$titulo_obtenido = $_POST["titulo_obtenido"];
+$year_graduation = $_POST["year_graduation"];
+$year_graduation = $_POST["year_graduation"];
+$proyectos_destacados= $_POST["proyectos_destacados"];
+
+
 
 
 
