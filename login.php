25a0884af9e85c48cbd239a3b4a48478f3476f1d
DonChambitasCompany <139256936+DonChambitasCompany@users.noreply.github.com>
2023-08-02 09:54:01 -0600

    Update login.php

--- login.php
+++ login.php
@@ -11,18 +11,19 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $password = hash('sha512', $password);
 
     try{
-        $conexion = new PDO('mysql:host=localhost;dbname=don_chambitas','root','75628491d');
+        $conexion = new PDO('mysql:host=localhost;dbname=don_chambitas','root','75628491d'); //conexión
     }  
     catch (PDOException $e){
         echo "Error:" .$e->getMessage();
     }
 
-    $statement = $conexion->prepare('SELECT * FROM cuentas where correo_electronico = :correo AND my_password = :pass');
+    $statement = $conexion->prepare('SELECT * FROM cuentas where correo_electronico = :correo AND my_password = :pass'); //verificar si existe la cuenta con contraseña ingresada
     $statement ->execute(array(':correo' => $correo,':pass' => $password ));
 
     $resultado = $statement->fetch();
     if ($resultado != false){
         $_SESSION['usuario'] = $correo;
+        $_SESSION['id_usuario'] = $resultado['id_cuenta']; // Guardar el ID del usuario en una variable de sesión
         header('Location: profiles/usuario.php');
     }
     else{
