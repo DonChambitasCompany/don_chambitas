a24b39056deb2c9c14d172069403a09177414588
DonChambitasCompany <139256936+DonChambitasCompany@users.noreply.github.com>
2023-08-02 19:52:07 -0600

    Update nombres.php

--- nombres.php
+++ nombres.php
@@ -10,19 +10,14 @@ if (isset($_SESSION['usuario']) && isset($_SESSION['id_usuario'])) {
 
     // Utiliza el ID del usuario almacenado en $_SESSION['id_usuario'] en la consulta.
     $idUsuario = $_SESSION['id_usuario'];
-    $consulta_nombre = $conexion->prepare('SELECT nombre FROM usuarios WHERE cuenta_id = :idUsuario');
+    $consulta_nombre = $conexion->prepare('SELECT nombre_usuario FROM usuarios WHERE cuenta_id = :idUsuario');
     $consulta_nombre->execute(array(':idUsuario' => $idUsuario));
 
     // Procesar el resultado de la consulta
     if ($fila = $consulta_nombre->fetch()) {
-        $nombreUsuario = $fila['nombre'];
+        $nombreUsuario = $fila['nombre_usuario'];
         // Imprimir nombre usuario
         echo $nombreUsuario;
-    } else {
-        echo "No se encontró ningún usuario con el ID proporcionado.";
     }
-} else {
-    // En caso de que no haya sesión activa o no se haya definido el ID del usuario en $_SESSION.
-    echo "No se ha iniciado sesión o no se ha definido el ID de usuario.";
 }
 ?>
