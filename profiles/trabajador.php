6f214d93c47cf1a17964ee474b3ed4f6ed76add8
DonChambitasCompany <139256936+DonChambitasCompany@users.noreply.github.com>
2023-08-06 15:34:59 -0600

    Update trabajador.php

--- profiles/trabajador.php
+++ profiles/trabajador.php
@@ -2,7 +2,7 @@
 
 session_start();
 if (!isset($_SESSION['usuario'])) { //comprobar si ya está la sesión
-    header('Location: ../register/login_trabajador.php');
+    header('Location: ../register/login.php');
     exit();
 }
 ?>
