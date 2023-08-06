b3d417982bd08665b5d063d8a8a4c1d707f2c504
DonChambitasCompany <139256936+DonChambitasCompany@users.noreply.github.com>
2023-08-06 16:31:06 -0600

    Update login.php

--- register/login.php
+++ register/login.php
@@ -45,7 +45,7 @@ if (isset($_SESSION['usuario'])){//comprobar si ya está la sesión
                             
                             <a href="../index.php"><button type="button" class="buton buton-black">Volver</button></a>
                             <br><br>
-                            <a href="register_cliente.html"><button type="button" class="buton buton-registro">¿Aún no tienes una cuenta?
+                            <a href="register_cliente.php"><button type="button" class="buton buton-registro">¿Aún no tienes una cuenta?
                                 <br><strong>Registrate como Cliente</strong></button></a>
                             
                             <?php if (!empty($errores)):?>
