9fb44aa160ae678fbeae736f6048d7552395d450
DonChambitasCompany <139256936+DonChambitasCompany@users.noreply.github.com>
2023-08-06 17:20:32 -0600

    Update trabajador.php

--- profiles/trabajador.php
+++ profiles/trabajador.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['usuario'])) { //comprobar si ya está la sesión
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>user-profile</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    
+    <link rel="icon" href="assets/logo_principal.png" type="image/png">
     <link rel="stylesheet" href="profile.css" type="text/css">
 </head>
 
