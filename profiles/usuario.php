0fe6a6ff2801bde19da46aef04b8eaa39c6a0a13
DonChambitasCompany <139256936+DonChambitasCompany@users.noreply.github.com>
2023-08-06 17:20:40 -0600

    Update usuario.php

--- profiles/usuario.php
+++ profiles/usuario.php
@@ -15,6 +15,7 @@ if (!isset($_SESSION['usuario'])) { //comprobar si ya está la sesión
     <title>user-profile</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="stylesheet" href="profile.css" type="text/css">
+    <link rel="icon" href="assets/logo_principal.png" type="image/png">
 </head>
 
 <body>
