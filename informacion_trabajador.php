c2e899aa936b816d81f84a888b4d7b3ca5ece043
DonChambitasCompany <139256936+DonChambitasCompany@users.noreply.github.com>
2023-08-06 18:43:50 -0600

    Update informacion_trabajador.php

--- informacion_trabajador.php
+++ informacion_trabajador.php
@@ -33,40 +33,6 @@ if (isset($_SESSION['usuario']) && isset($_SESSION['id_usuario'])) {
         $numero = $fila['telefono'];
 
 
-        $info = <<< EDO
-        <body align="center">
-    <div class="profile__cont">
-        <h1 align="center">Tus datos</h1><br><br>
-            <div align = "center">
-        
-                <table class="table-primary">
-                    <thead>
-                        <th>Nombre</th>
-                        <th>Apellido Paterno</th>
-                        <th>Apellido Materno</th>
-                        <th>Edad</th>
-                        <th>Correo Electrónico</th>
-                        <th>Telefono Celular</th>
-                    </thead>
-                    <tbody>
-                        <td> $nombre</td>
-                        <td> $ap</td>
-                        <td> $am</td>
-                        <td> $edad</td>
-                        <td> $email</td>
-                        <td> $numero</td>
-                    </tbody>
-                </table>
-        
-            <br><br><br>
-            <button type="submit" style="background-color: orange;"><a style="text-decoration: none; color: #ffff;" href="usuario.php"><br>&nbsp;&nbsp;&nbsp;&nbsp;Volver&nbsp;&nbsp;&nbsp;&nbsp;<br></a></button>
-    </div>
-</body>
-EDO;
-
-        echo $info;
-        
-
     } else {
         $nombre = "Problemas";
         $ap = "Con";
@@ -75,38 +41,6 @@ EDO;
         $email = "De";
         $numero = "Datos";
         echo "No se encontraron registros.";
-        $info = <<< EDO
-        <body align="center">
-    <div class="profile__cont">
-        <h1 align="center">Tus datos</h1><br><br>
-            <div align = "center">
-        
-                <table class="table-primary">
-                    <thead>
-                        <th>Nombre</th>
-                        <th>Apellido Paterno</th>
-                        <th>Apellido Materno</th>
-                        <th>Edad</th>
-                        <th>Correo Electrónico</th>
-                        <th>Telefono Celular</th>
-                    </thead>
-                    <tbody>
-                        <td>$nombre</td>
-                        <td>$ap</td>
-                        <td>$am</td>
-                        <td>$edad</td>
-                        <td>$email</td>
-                        <td>$numero</td>
-                    </tbody>
-                </table>
-        
-            <br><br><br>
-            <button type="submit" style="background-color: orange;"><a style="text-decoration: none; color: #ffff;" href="usuario.php"><br>&nbsp;&nbsp;&nbsp;&nbsp;Volver&nbsp;&nbsp;&nbsp;&nbsp;<br></a></button>
-    </div>
-</body>
-EDO;
-
-        echo $info;
     }
     
 } 
