e669b53589de23dc09891e50b63038dfd5198392
DonChambitasCompany <139256936+DonChambitasCompany@users.noreply.github.com>
2023-08-08 19:42:15 -0600

    Update nombres.php

--- nombres.php
+++ nombres.php
@@ -1,12 +1,9 @@
 <?php
 // Verifica si existe una sesión activa y si el ID del usuario está disponible en $_SESSION.
 if (isset($_SESSION['usuario']) && isset($_SESSION['id_usuario'])) {
-    try {
-        $conexion = new PDO('mysql:host=65.99.225.55;dbname=agenc158_don_chambitas','agenc158_ivan','chambitas2023');
-        
-    } catch (PDOException $e) {
-        echo "Error:" . $e->getMessage();
-    }
+    
+        require 'pdo.php';
+
 
     // Utiliza el ID del usuario almacenado en $_SESSION['id_usuario'] en la consulta.
     $idUsuario = $_SESSION['id_usuario'];
